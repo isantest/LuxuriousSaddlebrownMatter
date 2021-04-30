1b08fc09b03ecb5c0731446fd43c7762ab3dc49d
bellenunes24 <>
2021-04-30 19:55:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame #importa o pygame
+
+pygame.init()  #ininicaliza o módulo do pygame
+
+dis_width = 600  #largura da tela
+dis_height = 400  #altura da tela
+
+dis = pygame.display.set_mode((dis_width, dis_height))  #define o tamanho da tela para o pygame
+
+pygame.display.set_caption('Meu jogo')  # Define o titulo que aparecerá na janela
+
+#Função que sera o loop do jogo
+def gameLoop():
+
+  while True:
+    continue
+
+gameLoop()
