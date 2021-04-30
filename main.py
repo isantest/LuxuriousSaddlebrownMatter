a146da3e6426c52eba098430b9026900b2073444
bellenunes24 <>
2021-04-30 22:02:45 +0000

    atualizações na posição do quadrado e circulo

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ dis_height = 400  #altura da tela
 
 dis = pygame.display.set_mode((dis_width, dis_height))  #define o tamanho da tela para o pygame
 
-pygame.display.set_caption('Meu jogo')  # Define o titulo que aparecerá na janela
+red = (219, 31, 31)
+green = (44, 219, 31)
+
+square_size = 20
+circle_radius = 10
+
+position_x = 200
+position_y = 100
 
 #Função que sera o loop do jogo
 def gameLoop():
@@ -16,12 +23,18 @@ def gameLoop():
     #Loop principal que ira rodar o jogo
     while not game_over:
 
+      pygame.display.update()  #Faz atualização da tela
+
       for event in pygame.event.get():  #Captura as teclas que foram pressionadas
 
         if event.type == pygame.QUIT: #Verifica se o evento foi para finalizar
 
           game_over = True  #Marcamos que o jogo foi finalizado para sair do jogo
 
-    pygame.quit()
+      pygame.draw.rect(dis, red, [position_x, position_y, square_size, square_size])
+          
+      pygame.draw.circle(dis, green,(position_x + 200, position_y), circle_radius)    
+
+    pygame.quit()   #Quiting Pygame
 
 gameLoop()
