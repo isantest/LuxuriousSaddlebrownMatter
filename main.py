1f034e1f8c8e84b220c0d7f9785a8a81ffa6a456
bellenunes24 <>
2021-04-30 22:46:58 +0000

    Foi adicionado as posições iniciais, deletado o circulo e definidos o tamanho e a velocidade do quadrado

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ import pygame #importa o pygame
 
 pygame.init()  #ininicaliza o módulo do pygame
 
+red = (219, 31, 31)
+black = (0, 0, 0)
+
+square_size = 20
+
+clock = pygame.time.Clock()  #determina os frames por segundo
+speed = 10 # velocidade que o desenho ira se mover
 dis_width = 600  #largura da tela
 dis_height = 400  #altura da tela
 
 dis = pygame.display.set_mode((dis_width, dis_height))  #define o tamanho da tela para o pygame
 
-red = (219, 31, 31)
-green = (44, 219, 31)
-
-square_size = 20
-circle_radius = 10
-
-position_x = 200
-position_y = 100
+pygame.display.set_caption('---- Meu Jogo ----')
 
 #Função que sera o loop do jogo
 def gameLoop():
     game_over = False
+
+    #Posição inicial
+    position_x = dis_width/2
+    position_y = 0
 
     #Loop principal que ira rodar o jogo
     while not game_over:
@@ -33,7 +37,11 @@ def gameLoop():
 
       pygame.draw.rect(dis, red, [position_x, position_y, square_size, square_size])
           
-      pygame.draw.circle(dis, green,(position_x + 200, position_y), circle_radius)    
+      position_y += speed
+
+      pygame.display.update()
+
+      clock.tick(speed)  #define para ser 10 frames por segundo no maximo
 
     pygame.quit()   #Quiting Pygame
 
